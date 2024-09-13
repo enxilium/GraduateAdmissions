e92ac35589014b9c96b1ccd1c499866d539f2aef
Cyclxne <cyclxxne@outlook.com>
2024-09-13 18:05:50 -0400

    Added template for flask and exported model into a file to prepare for deployment

--- .ipynb_checkpoints/main-checkpoint.py
+++
@@ -1,4 +0,0 @@
-%pylab inline
-from matplotlib.pyplot import style
-style.use('https://raw.githubusercontent.com/JoseGuzman/minibrain/master/minibrain/paper.mplstyle')
-import pandas as pd
---
+++ README.md
@@ -0,0 +1,1 @@
+A multiple linear regression model trained using Tensorflow, with the purpose of predicting graduate program admission chances.
